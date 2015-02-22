574982c5709836442bb40aab736d72838d1b1fba
sanjsanj <sanjsanj@hotmail.com>
2015-02-22 19:07:18 +0000

    First commit

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+name = 'Zed A. Shaw'
+age = 35
+height = 74
+weight = 180
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall, which is about #{height * 2.2} centimetres."
+puts "He's #{weight} pounds heavy, which is about #{weight / 2} kilograms."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes.downcase} eyes and #{hair.downcase} hair."
+puts "His teeth are usually #{teeth.downcase} depending on the coffee."
+
+
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
