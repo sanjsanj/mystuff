d7c9f55614fbe02bc5ac3a162fb34ec5c1c90d88
sanjsanj <sanjsanj@hotmail.com>
2015-02-23 11:09:23 +0000

    Complete exercise 7

---
+++ ex7.rb
@@ -0,0 +1,27 @@
+# put a string
+puts "Mary had a little lamb."
+# put a string containing a variable which contains a defined string instead of external variable, defined by '' marks
+puts "Its fleece was white as #{'snow'}."
+# put a string
+puts "And everywhere that Mary went."
+# put the . character 10 times, same as .times 10
+puts "." * 10
+
+# Define 12 variables as single letter strings
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# print first 6 variables, apparently they do not break after the print
+print end1 + end2 + end3 + end4 + end5 + end6
+# put string containing last 6 variables added to eachother
+puts end7 + end8 + end9 + end10 + end11 + end12
