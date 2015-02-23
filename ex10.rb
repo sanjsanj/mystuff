231ec0f9391da120d8cd44f4d1d8bd8d071ea6e8
sanjsanj <sanjsanj@hotmail.com>
2015-02-23 17:25:34 +0000

    Complete exercise 10

---
+++ ex10.rb
@@ -0,0 +1,20 @@
+# define var tabbed in
+tabby_cat = "\tI'm tabbed in."
+# define var with split line
+persian_cat = "I'm split\non a line."
+# define var with escape slash to make forward slashes appear
+backslash_cat = "I'm \\ a \\ cat."
+
+# define var on multilines with tabs and line break
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+# output previously defined vars
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
