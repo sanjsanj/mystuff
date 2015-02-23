dc9553df6f6297d57c3c233ffc40a018ebac1c25
sanjsanj <sanjsanj@hotmail.com>
2015-02-23 12:17:11 +0000

    Complete exercise 9

---
+++ ex9.rb
@@ -0,0 +1,16 @@
+# define var days with text string
+days = "Mon Tue Wed Thu Fri Sat Sun"
+# define var months with new line break text string
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# put string of our 2 defined vars
+puts "Here are the days : #{days}"
+puts "Here are the months: #{months}"
+
+# put string opened with """ to allow multilines and closed with """
+puts """
+There's something going in here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
