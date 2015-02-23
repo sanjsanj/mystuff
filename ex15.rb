f938893423555591901d00fe8219396edcdcbe18
sanjsanj <sanjsanj@hotmail.com>
2015-02-23 18:58:20 +0000

    Complete exercise 15

---
+++ ex15.rb
@@ -0,0 +1,22 @@
+# define a var as the first argument in CLI
+filename = ARGV.first
+
+# telling the txt var to open a file
+txt = open(filename)
+
+# output what file you are reading
+puts "Here's your file #{filename}"
+# print that file
+print txt.read
+
+# close the file
+txt.close
+
+print "Type the filename again: "
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read
+
+txt_again.close
