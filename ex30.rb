5e88f6c26a8202bfe512abbec5fd784ce1f948f1
sanjsanj <sanjsanj@hotmail.com>
2015-02-25 10:04:35 +0000

    Complete exercise 30

--- ex30.rb
+++ ex30.rb
@@ -1,13 +1,18 @@
+# set 3 vars to be used in evaluation below
 people = 30
 cars = 40
 trucks = 15
 
+# if cars is greater than people
 if cars > people
   puts "We should take the cars."
+  # or else if cars are less than people
 elsif cars < people
   puts "We should not take the cars."
+  # or else do this
 else
   puts "We can't decide."
+# end the if statement
 end
 
 if trucks > cars
