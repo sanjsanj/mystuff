ba61e476de07e366d32a26301578f85508b17189
sanjsanj <sanjsanj@hotmail.com>
2015-02-25 14:41:31 +0000

    Progress with exercise 32

---
+++ ex32.rb
@@ -0,0 +1,30 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apple', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dime', 3, 'quarters']
+
+# This first for-loop goes through a list in the traditional style, and should never be used
+for number in the_count
+  puts "This is count #{number}"
+end
+
+
+
+# Prefered style for ruby
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+end
+
+# Another prefered style for ruby, going through mixed lists
+change.each {|i| puts "I got #{i}" }
+
+# build an empty list
+elements = []
+
+# use the range operator to do 0 to 5 counts.  ... excludes end value
+(0..5).each do |i|
+  puts "adding #{i} to the list."
+  elements.push(i)
+end
+
+# print them out too
+elements.each {|i| puts "Element was: #{i}" }
