d9f76caf657faa2da091c9f95319031a80f23b8a
sanjsanj <sanjsanj@hotmail.com>
2015-02-26 08:41:29 +0000

    my numbers game v0.04

--- ex36_my_numbers_game.rb
+++ ex36_my_numbers_game.rb
@@ -2,12 +2,9 @@ start_point = 1 # starting number for computer to pick from
 end_point = 10 # end number for computer to pick to
 number = rand(end_point) + start_point # random number the computer chooses
 guesses = 3 # the number of guesses the player is allowed
-prompt = "Guess > " # the prompt for player to input their guess
+prompt = "Your guess: " # the prompt for player to input their guess
 attempts = 0 # number of attempts the player has made
 
-def winner(funcnum)
-  puts "#{funcnum} was right, you won!"
-end
 
 puts """
 Hi, play my guessing game!
@@ -16,11 +13,11 @@ I'll give you #{guesses} guesses.'
 """
 
 (0...guesses).each do |i|
-  # print number  # for testing purposes
+  print number  # for testing purposes
   print prompt
   uguess = $stdin.gets.chomp
     if uguess.to_i == number
-      winner(number)
+      puts "#{number} was right, you won!"
       break
     elsif attempts + 1 >= guesses
       puts "Sorry, you lose!  Play again."
