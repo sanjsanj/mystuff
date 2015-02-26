04ae3f3bc1122edc368dd23df56318990a6c3a2e
sanjsanj <sanjsanj@hotmail.com>
2015-02-26 08:29:18 +0000

    my numbers game v0.03

--- ex36_my_numbers_game.rb
+++ ex36_my_numbers_game.rb
@@ -1,30 +1,32 @@
-start_point = 1
-end_point = 10
-number = rand(end_point) + start_point
-guesses = 3
-prompt = "> "
-attempts = 0
+start_point = 1 # starting number for computer to pick from
+end_point = 10 # end number for computer to pick to
+number = rand(end_point) + start_point # random number the computer chooses
+guesses = 3 # the number of guesses the player is allowed
+prompt = "Guess > " # the prompt for player to input their guess
+attempts = 0 # number of attempts the player has made
 
 def winner(funcnum)
   puts "#{funcnum} was right, you won!"
 end
 
-# greet and instruct
 puts """
 Hi, play my guessing game!
 I'll think of a number between #{start_point} and #{end_point} and you guess what it is.
 I'll give you #{guesses} guesses.'
 """
 
-# ask guesses times
 (0...guesses).each do |i|
-  print number  # for testing purposes
+  # print number  # for testing purposes
   print prompt
   uguess = $stdin.gets.chomp
     if uguess.to_i == number
       winner(number)
       break
+    elsif attempts + 1 >= guesses
+      puts "Sorry, you lose!  Play again."
+      break
     else
+      attempts += 1
       puts "Sorry, try again."
     end
 end
