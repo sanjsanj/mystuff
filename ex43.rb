a933297015d1eb0077517a8ce68e4e422605015f
sanjsanj <sanjsanj@hotmail.com>
2015-02-27 17:21:07 +0000

    Progress on exercise 43

--- ex43.rb
+++ ex43.rb
@@ -1,14 +1,26 @@
 class Scene
   def enter()
+    puts "This scene is not yet configured.  Subclass it and implement enter()."
+    exit(1)
   end
 end
 
 class Engine
   
   def initialize(scene_map)
+    @scene_map = scene_map
   end
   
   def play()
+    current_scene = @scene_map.opening_scene()
+    last_scene = @scene_map.next_scene('finished')
+    
+    while current_scene != last_scene
+      next_scene_name = current_scene.enter()
+      current_scene = @scene_map.next_scene(next_scene_name)
+    end
+    
+    current_scene.enter()
   end
 end
 
